1e299dbdf91e15e0fea3ded65252be263073ff66
Saad Ali <saadalikc02@gmail.com>
2021-04-22 03:39:03 +0500

    Table updated

---
+++ public/repository/getCalls.js
@@ -0,0 +1,23 @@
+$.getScript('/globalConfig/config.js')
+$(document).ready(function () {
+
+    $.get(`${_baseUrl}/charges-by-physicians`,(data, status)=> {
+        console.log(status);
+        const tableBody = $('#t-data')
+        console.log(data)
+        data.forEach(item => {
+            tableBody.append(`
+            <tr>
+            <td>${item.ClaimId}</td>
+            <td>${item.Patient}</td>
+            <td>${item.DOS}</td>
+            <td>${item.Charges}</td>
+            <td>${item.PaidAmt}</td>
+            <td>${item.Provider}</td>
+            <td>${item.Location}</td>
+        </tr>
+            `)
+        })
+    })
+
+})
