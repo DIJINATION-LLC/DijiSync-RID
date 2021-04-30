67e904f85fef3c4bb90a1b3b7ad80f16e80fd26a
Saad Ali <saadalikc02@gmail.com>
2021-05-01 03:01:11 +0500

    Updated Excel file Functionality

--- public/repository/getCalls.js
+++ public/repository/getCalls.js
@@ -1,18 +1,18 @@
-$.getScript('/globalConfig/config.js')
+// $.getScript('/globalConfig/config.js')
 $(document).ready(function () {
 
     $.get(`${_baseUrl}/charges-by-physicians`,(data, status)=> {
-        console.log(status);
+        console.log("get call working");
         const tableBody = $('#t-data')
         data.forEach(item => {
             tableBody.append(`
             <tr>
             <td>${item.ClaimId}</td>
-            <td>${item.Patient}</td>
+            <td>${item.Patient + ", " + item.patientLastName}</td>
             <td>${item.DOS}</td>
             <td>${item.Charges}</td>
             <td>${item.PaidAmt}</td>
-            <td>${item.Provider}</td>
+            <td>${item.Provider + ", " + item.providerLastName}</td>
             <td>${item.Location}</td>
         </tr>
             `)
--- public/repository/postCalls.js
+++ public/repository/postCalls.js
@@ -1,4 +1,6 @@
 
+
+
 $.getScript('/globalConfig/config.js')
 $(document).ready(function () {
 
@@ -16,7 +18,7 @@ $(document).ready(function () {
         // const Gender = $('#Gender').val();
         // const Age = $('#Age').val();
 
-        if(Email != "" || Password != ""){
+        if (Email != "" || Password != "") {
             $.post(`${_baseUrl}/auth/local`, { identifier: Email, password: Password }, (data, status) => {
                 console.log(status);
                 console.log(data);
@@ -43,7 +45,7 @@ $(document).ready(function () {
         // const Gender = $('#Gender').val();
         // const Age = $('#Age').val();
 
-        $.post(`${_baseUrl}/auth/local/register`, { username: Username, email: Email , password: Password }, (data, status) => {
+        $.post(`${_baseUrl}/auth/local/register`, { username: Username, email: Email, password: Password }, (data, status) => {
             console.log(status);
             console.log(data);
             if (status == "success") {
@@ -55,8 +57,90 @@ $(document).ready(function () {
 
     $('#btnFile').click(function (e) {
 
-        e.preventDefault()
         console.log("Hello")
-    })
+        
+        var file = document.getElementById('myFile').files[0];
+        console.log(file);
+        const fileData = new FormData()
+        fileData.append('files' , file)
+        $.ajax({
+
+            processData: false,
+            contentType: false,
+            cache: false,
+            enctype: 'multipart/form-data',
+            type: "post",
+            url: `${_baseUrl}/upload`,
+            data: fileData,
+            success: function (data) {
+                $.post(`${_baseUrl}/charges-by-physicians/upload`, {data}, (d, status) => {
+                    console.log(status);
+                    console.log(d);
+                    if (status == "success") {
+                       console.log("Second Call success");
+                    }
+                })
+            },
+            error: function (data) {
+                console.log('Error:', data);
+            }
+        // $.post(`${_baseUrl}/upload`, fileData)
+        // .then(res => {
+            //     console.log(res);
+            // })
+        })
+
+        
+            e.preventDefault()
+
+
 
 })
+})
+
+
+
+    // const workSheetsFromFile = xlsx.parse(`${__dirname}/Charges Apr.xls`, { cellDates: true, dateNF: 'dd/mm/yy', cellStyles: true });
+
+    // var rows = [];
+    // var writeStr = "";
+
+    // //looping through all sheets
+    // for (var i = 0; i < workSheetsFromFile.length; i++) {
+    //     var sheet = workSheetsFromFile[i];
+    //     //loop through all rows in the sheet
+    //     for (var j = 0; j < sheet['data'].length; j++) {
+    //         //add the row to the rows array
+    //         rows.push(sheet['data'][j]);
+    //     }
+    // }
+
+    // //creates the csv string to write it to a file
+    // for (var i = 0; i < rows.length; i++) {
+    //     if (i == 0) {
+    //         rows[0].splice(3, 0, 'patientLastName')
+    //         rows[0].splice(9, 0, 'providerLastName')
+    //         writeStr += rows[i].join(",") + "\n";
+    //     } else {
+    //         writeStr += rows[i].join(",") + "\n";
+    //     }
+    // }
+
+    // console.log(writeStr);
+
+    // var file = document.getElementById('myFile').files[0];
+    // if (file) {
+    //   var reader = new FileReader();
+
+    //   reader.onload = function (e) {
+    //     var data = e.target.result;
+    //                  var workbook = XLSX.read(data, {//XLSX.read() method will return a workbook object
+    //       type: 'binary'
+    //     });
+    //     readWorkbook(workbook);
+    //   };
+    //   reader.readAsBinaryString(file);
+
+    // } else {
+    //              alert('Please select the file first');
+    // }
